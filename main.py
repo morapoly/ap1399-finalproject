5b7d276870a2d0b1dfb5dc0c565ffd12d07de96f
Reza Moradi <morapoly@aut.ac.ir>
2021-02-06 20:50:26 +0330

    c4 | background image added.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import sys
 import os
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QImage, QPalette, QBrush
+
+# from PyQt5.QtWidgets import *
 
 
 def quit_bottun_callback():
@@ -16,6 +20,13 @@ class IntroWindow(QMainWindow, Form):
     def __init__(self):
         super(IntroWindow, self).__init__()
         self.setupUi(self)
+
+        #  setting beckground image
+        backgroun_image = QImage("./stuff/picture5.png")
+        backgroun_image_scaled = backgroun_image.scaled(QSize(879, 444))
+        palette = QPalette()
+        palette.setBrush(QPalette.Window, QBrush(backgroun_image_scaled))
+        self.setPalette(palette)
 
 
 if __name__ == "__main__":
