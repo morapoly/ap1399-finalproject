76912a3a8081dc4b0d4374a8b266a41cfc348a1b
ShayanBajelan <shayan.bajelan.98@gmail.com>
2021-02-12 03:58:10 +0330

    c10 | Patient Part And Reservasion Done

--- Login2.py
+++ Login2.py
@@ -20,8 +20,9 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5 import QtGui, QtPrintSupport
 
-import sys
+import sys, sqlite3
 
+doctors = ["Jahanshahi", "Amini", "Ghobari", "Ghodsifar"]
 
 class Login(QDialog):
     def __init__(self, parent=None):
@@ -31,7 +32,7 @@ class Login(QDialog):
         self.textName = QLineEdit(self)
         self.textPass = QLineEdit(self)
         self.textPass.setEchoMode(QLineEdit.Password)
-        self.doctors = ["Jahanshahi", "Fordo", "Wolf", "Abbath"]
+        self.doctors = doctors
 
         username = QLabel("Username")
         pw = QLabel("Password")
@@ -54,18 +55,9 @@ class Login(QDialog):
         loginbox.addWidget(self.buttonRegister, 3, 0)
         loginbox.addWidget(self.buttonLogin, 3, 1)
 
-        self.setWindowTitle("SNCF Login")
+        self.setWindowTitle("Login")
 
         self.buttonLogin.clicked.connect(self.login)
-
-    # def handleLogin(self):
-    #     if (self.textName.text() == 'foo' and
-    #             self.textPass.text() == 'bar'):
-    #         self.accept()
-
-    #        else:
-    #            QtGui.QMessageBox.warning(
-    #                self, 'Error', 'Bad user or password')
 
     def handleRegister(self):
         self.doctors.append(self.textName.text())
@@ -81,16 +73,81 @@ class Login(QDialog):
             QMessageBox.warning(self, "Error", "Wrong Password Or Undefined Name!")
 
 
-#        else:
-#            QtGui.QMessageBox.warning(
-#                self, 'Error', 'Bad user or password')
+class PatientLogin(QDialog):
+    def __init__(self, parent=None):
+        super(PatientLogin, self).__init__(parent)
+
+        self.name = "No body"
+        self.label = QLabel(self)
+        self.label.setText("Notify that your password is your ID number!")
+        self.textName = QLineEdit(self)
+        self.textPass = QLineEdit(self)
+        self.textPass.setEchoMode(QLineEdit.Password)
+
+        # patients as users
+
+        username = QLabel("Username")
+        pw = QLabel("Password")
+
+        self.buttonLogin = QPushButton("Login", self)
+
+        loginbox = QGridLayout(self)
+        loginbox.setSpacing(10)
+
+        loginbox.addWidget(username, 1, 0)
+        loginbox.addWidget(self.textName, 1, 1)
+        loginbox.addWidget(self.label, 0, 0)
+
+        loginbox.addWidget(pw, 2, 0)
+        loginbox.addWidget(self.textPass, 2, 1)
+
+        loginbox.addWidget(self.buttonLogin, 3, 1)
+
+        self.setWindowTitle("Login")
+
+        self.buttonLogin.clicked.connect(self.login)
+
+    def login(self):
+        self.username = self.textName.text()
+        self.name = self.username
+        self.password = ""
+
+        count1 = 0
+        count2 = 0
+        for dr in doctors:
+            try:
+                self.conn = sqlite3.connect("database.db")
+                self.curser = self.conn.cursor()
+                result = self.curser.execute(
+                    "SELECT * from " + str(dr) + f" WHERE name='{str(self.username)}'"
+                )
+                row = result.fetchone()
+                self.password = row[2]
+            except:
+                count1 = count1 + 1
+            try:
+                self.conn2 = sqlite3.connect("database2.db")
+                self.curser2 = self.conn2.cursor()
+                result2 = self.curser2.execute(
+                    "SELECT * from " + str(dr) + f" WHERE name='{str(self.username)}'"
+                )
+                row = result2.fetchone()
+                self.password = row[2]
+            except:
+                count2 = count2 + 1
+
+        if count1 == len(doctors) and count2 == len(doctors):
+            QMessageBox.warning(self, "Error", "Undefined Name!")
+        else:
+            if self.textPass.text() == self.password:
+                self.accept()
+            else:
+                QMessageBox.warning(self, "Error", "Wrong Password!")
 
 
 class Window(QMainWindow):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
-        # self.ui = Ui_MainWindow()
-        # self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
@@ -104,5 +161,3 @@ if __name__ == "__main__":
         window = Window()
         window.show()
         sys.exit(app.exec_())
-#    else:
-#        pass
